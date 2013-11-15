25c41c93d5741e9075356df9063401fa3e2a0807
Brian Stanwyck <stanwyck@bignerdranch.com>
2013-11-15 11:50:00 -0500

    Add options script

---
+++ options.zsh
@@ -0,0 +1,4 @@
+setopt ignoreeof    # ^D doesn't log out
+setopt rmstarsilent # No confirm for rm with globs
+setopt auto_list    # List ambiguous completion choices
+unsetopt correct    # Don't correct my spelling!
